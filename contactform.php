9b1534aeb3f6a632443d0b4f8009654ddcda5255
KobyFX <62533727+KobyFX@users.noreply.github.com>
2020-07-28 01:01:08 +1000

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -7,7 +7,7 @@ if (isset($_POST['submit'])) {
     $mailFrom = $_POST['mail'];
     $message = $_POST['message'];
 
-    $mailTo = "koby@techemy.com";
+    $mailTo = "kobypaull@gmail.com";
     $headers = "From: ".$mailFrom;
     $txt = "You have received an e-mail from ".$name.".\n\n".$message;
     
