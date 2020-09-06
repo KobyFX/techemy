4f24959e02b45f13891ce4c981aeb3dc1c7e5a0c
KobyFX <62533727+KobyFX@users.noreply.github.com>
2020-09-06 20:48:03 +1000

    Update contact.php

--- contact.php
+++ contact.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="shortcut icon" type="image/png" href="img/favicon.png">
-    <title>Contact from</title>
+    <title>Techemy &nbsp;&#8226;&nbsp; Contact Us</title>
 </head>
 
 <body>
